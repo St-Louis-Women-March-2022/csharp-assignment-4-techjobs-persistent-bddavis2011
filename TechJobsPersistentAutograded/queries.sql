f57d752cf1df05bcc968669f585a0df1ca64530a
Brittany Davis <bddavis2011@gmail.com>
2022-12-05 09:41:08 -0600

    finished assignment

--- TechJobsPersistentAutograded/queries.sql
+++ TechJobsPersistentAutograded/queries.sql
@@ -5,3 +5,7 @@ where table_name = "jobs";
 select employers.Name as Employer, Location from employers
 where Location = "St. Louis";
 --Part 3
+select skills.Description, skills.Name from skills
+inner join jobskills on skills.Id = jobskills.SkillId
+group by skills.Name
+order by skills.Name asc;
